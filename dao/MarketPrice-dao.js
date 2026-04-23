4f980eb8849e5388f9fae8fa4a324f773e17aaac
Biman <bimangunarathne@outlook.com>
2026-04-23 11:23:58 +0530

    update the code

--- dao/MarketPrice-dao.js
+++ dao/MarketPrice-dao.js
@@ -298,6 +298,7 @@ exports.getAllMarketPriceAgroDAO = (crop, grade, search, centerId, companyId) =>
     const params = [];
     const countParams = [];
     console.log(centerId, companyId);
+    
     // First, get the companyCenterId based on centerId and companyId
     let companyCenterSql = `
       SELECT id 
@@ -320,7 +321,7 @@ exports.getAllMarketPriceAgroDAO = (crop, grade, search, centerId, companyId) =>
       }
 
       const companyCenterId = ccResults[0].id;
-      console.log('this is the selelcted companycenter ID',companyCenterId);
+      console.log('Selected companyCenter ID:', companyCenterId);
 
       // Now build the main queries using the companyCenterId
       let countSql = `
@@ -341,11 +342,13 @@ exports.getAllMarketPriceAgroDAO = (crop, grade, search, centerId, companyId) =>
           ms.price,
           ms.updatedPrice,
           ms.updateAt,
-          m.createdAt AS updateAt
+          xh.createdAt AS updateAt,
+          xh.xlName
         FROM marketpriceserve ms
         JOIN marketprice m ON ms.marketPriceId = m.id
         JOIN plant_care.cropvariety cv ON m.varietyId = cv.id
         JOIN plant_care.cropgroup cg ON cv.cropGroupId = cg.id
+        LEFT JOIN collection_officer.xlsxhistory xh ON m.xlindex = xh.id 
         WHERE ms.companyCenterId = ?
       `;
 
@@ -369,8 +372,8 @@ exports.getAllMarketPriceAgroDAO = (crop, grade, search, centerId, companyId) =>
       }
 
       if (search) {
-        sql += " AND cg.cropNameEnglish LIKE ? OR cv.varietyNameEnglish LIKE ?";
-        countSql += " AND cg.cropNameEnglish LIKE ? OR cv.varietyNameEnglish LIKE ?";
+        sql += " AND (cg.cropNameEnglish LIKE ? OR cv.varietyNameEnglish LIKE ?)";
+        countSql += " AND (cg.cropNameEnglish LIKE ? OR cv.varietyNameEnglish LIKE ?)";
         const searchQuery = `%${search}%`;
         params.push(searchQuery, searchQuery);
         countParams.push(searchQuery, searchQuery);
