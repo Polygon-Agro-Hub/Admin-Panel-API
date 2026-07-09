05030372a2b72dddbc5ca6298b52c274a80a1798
BimanGunarathne <bimangunarathne@outlook.com>
2026-07-09 12:01:16 +0530

    update the code

--- dao/MarketPlace-dao.js
+++ dao/MarketPlace-dao.js
@@ -2145,11 +2145,127 @@ exports.createDefinePackageItemsDao = (definePackageId, products) => {
   });
 };
 
+// exports.getAllWholesaleCustomersDao = (limit, offset, searchText, ratingFilter) => {
+//   return new Promise((resolve, reject) => {
+//     let countParms = [];
+//     let dataParms  = [];
+ 
+//     let countSql = `
+//       SELECT COUNT(*) AS total
+//       FROM marketplaceusers MP
+//       WHERE
+//         MP.buyerType = 'Wholesale'
+//         AND MP.isMarketPlaceUser = 1
+//     `;
+ 
+//     let dataSql = `
+//       SELECT
+//         MP.id,
+//         MP.title,
+//         MP.firstName,
+//         MP.lastName,
+//         MP.phoneCode,
+//         MP.phoneNumber,
+//         MP.cusId,
+//         MP.email,
+//         MP.created_at,
+//         MP.buildingType,
+//         MP.companyName,
+//         MP.companyPhoneCode,
+//         MP.companyPhone,
+//         MP.rateofCus,
+//         H.houseNo,
+//         H.streetName,
+//         H.city,
+//         A.buildingNo,
+//         A.buildingName,
+//         A.unitNo,
+//         A.floorNo,
+//         A.houseNo      AS AparthouseNo,
+//         A.streetName   AS ApartstreetName,
+//         A.city         AS Apartcity,
+//         (
+//           SELECT COUNT(*)
+//           FROM orders O
+//           LEFT JOIN processorders PO ON O.id = PO.orderId
+//           WHERE O.userId = MP.id
+//         ) AS totalOrders
+//       FROM marketplaceusers MP
+//       LEFT JOIN house      H ON MP.id = H.customerId AND MP.buildingType = 'House'
+//       LEFT JOIN apartment  A ON MP.id = A.customerId AND MP.buildingType = 'Apartment'
+//       WHERE
+//         MP.buyerType = 'Wholesale'
+//         AND MP.isMarketPlaceUser = 1
+//     `;
+ 
+//     // ── Rating filter ──────────────────────────────────────────────────────────
+//     if (ratingFilter) {
+//       countSql += ` AND MP.rateofCus = ? `;
+//       dataSql  += ` AND MP.rateofCus = ? `;
+//       countParms.push(ratingFilter);
+//       dataParms.push(ratingFilter);
+//     }
+ 
+//     // ── Search filter ──────────────────────────────────────────────────────────
+//     if (searchText) {
+//       const searchClause = `
+//         AND (
+//           CONCAT(MP.firstName, ' ', MP.lastName) LIKE ?
+//           OR MP.firstName    LIKE ?
+//           OR MP.lastName     LIKE ?
+//           OR MP.phoneNumber  LIKE ?
+//           OR MP.cusId        LIKE ?
+//           OR CONCAT(MP.phoneCode, ' - ', MP.phoneNumber) LIKE ?
+//           OR CONCAT(MP.phoneCode, '-',   MP.phoneNumber) LIKE ?
+//           OR CONCAT(MP.phoneCode,        MP.phoneNumber) LIKE ?
+//           OR REPLACE(CONCAT(MP.phoneCode, ' - ', MP.phoneNumber), ' ', '') LIKE ?
+//           OR REPLACE(CONCAT(MP.phoneCode, '-',   MP.phoneNumber), ' ', '') LIKE ?
+//         )
+//       `;
+//       countSql += searchClause;
+//       dataSql  += searchClause;
+ 
+//       const search             = `%${searchText}%`;
+//       const searchWithoutSpaces = `%${searchText.replace(/\s/g, '')}%`;
+//       const searchArgs = [
+//         search, search, search, search, search,
+//         search, search,
+//         searchWithoutSpaces, searchWithoutSpaces, searchWithoutSpaces,
+//       ];
+ 
+//       countParms.push(...searchArgs);
+//       dataParms.push(...searchArgs);
+//     }
+ 
+//     dataSql += ` ORDER BY MP.created_at DESC LIMIT ? OFFSET ? `;
+//     dataParms.push(limit, offset);
+ 
+//     marketPlace.query(countSql, countParms, (countErr, countResults) => {
+//       if (countErr) {
+//         console.error(countErr);
+//         return reject(countErr);
+//       }
+ 
+//       marketPlace.query(dataSql, dataParms, (dataErr, dataResults) => {
+//         if (dataErr) {
+//           console.error(dataErr);
+//           return reject(dataErr);
+//         }
+ 
+//         resolve({
+//           total: countResults[0].total,
+//           items: dataResults,
+//         });
+//       });
+//     });
+//   });
+// };
+ 
 exports.getAllWholesaleCustomersDao = (limit, offset, searchText, ratingFilter) => {
   return new Promise((resolve, reject) => {
     let countParms = [];
     let dataParms  = [];
- 
+
     let countSql = `
       SELECT COUNT(*) AS total
       FROM marketplaceusers MP
@@ -2157,7 +2273,7 @@ exports.getAllWholesaleCustomersDao = (limit, offset, searchText, ratingFilter) 
         MP.buyerType = 'Wholesale'
         AND MP.isMarketPlaceUser = 1
     `;
- 
+
     let dataSql = `
       SELECT
         MP.id,
@@ -2169,21 +2285,10 @@ exports.getAllWholesaleCustomersDao = (limit, offset, searchText, ratingFilter) 
         MP.cusId,
         MP.email,
         MP.created_at,
-        MP.buildingType,
         MP.companyName,
         MP.companyPhoneCode,
         MP.companyPhone,
         MP.rateofCus,
-        H.houseNo,
-        H.streetName,
-        H.city,
-        A.buildingNo,
-        A.buildingName,
-        A.unitNo,
-        A.floorNo,
-        A.houseNo      AS AparthouseNo,
-        A.streetName   AS ApartstreetName,
-        A.city         AS Apartcity,
         (
           SELECT COUNT(*)
           FROM orders O
@@ -2191,13 +2296,11 @@ exports.getAllWholesaleCustomersDao = (limit, offset, searchText, ratingFilter) 
           WHERE O.userId = MP.id
         ) AS totalOrders
       FROM marketplaceusers MP
-      LEFT JOIN house      H ON MP.id = H.customerId AND MP.buildingType = 'House'
-      LEFT JOIN apartment  A ON MP.id = A.customerId AND MP.buildingType = 'Apartment'
       WHERE
         MP.buyerType = 'Wholesale'
         AND MP.isMarketPlaceUser = 1
     `;
- 
+
     // ── Rating filter ──────────────────────────────────────────────────────────
     if (ratingFilter) {
       countSql += ` AND MP.rateofCus = ? `;
@@ -2205,7 +2308,7 @@ exports.getAllWholesaleCustomersDao = (limit, offset, searchText, ratingFilter) 
       countParms.push(ratingFilter);
       dataParms.push(ratingFilter);
     }
- 
+
     // ── Search filter ──────────────────────────────────────────────────────────
     if (searchText) {
       const searchClause = `
@@ -2224,7 +2327,7 @@ exports.getAllWholesaleCustomersDao = (limit, offset, searchText, ratingFilter) 
       `;
       countSql += searchClause;
       dataSql  += searchClause;
- 
+
       const search             = `%${searchText}%`;
       const searchWithoutSpaces = `%${searchText.replace(/\s/g, '')}%`;
       const searchArgs = [
@@ -2232,26 +2335,26 @@ exports.getAllWholesaleCustomersDao = (limit, offset, searchText, ratingFilter) 
         search, search,
         searchWithoutSpaces, searchWithoutSpaces, searchWithoutSpaces,
       ];
- 
+
       countParms.push(...searchArgs);
       dataParms.push(...searchArgs);
     }
- 
+
     dataSql += ` ORDER BY MP.created_at DESC LIMIT ? OFFSET ? `;
     dataParms.push(limit, offset);
- 
+
     marketPlace.query(countSql, countParms, (countErr, countResults) => {
       if (countErr) {
         console.error(countErr);
         return reject(countErr);
       }
- 
+
       marketPlace.query(dataSql, dataParms, (dataErr, dataResults) => {
         if (dataErr) {
           console.error(dataErr);
           return reject(dataErr);
         }
- 
+
         resolve({
           total: countResults[0].total,
           items: dataResults,
@@ -2260,7 +2363,6 @@ exports.getAllWholesaleCustomersDao = (limit, offset, searchText, ratingFilter) 
     });
   });
 };
- 
  
 exports.updateWholesaleCustomerRatingDao = (id, rateofCus) => {
   return new Promise((resolve, reject) => {
