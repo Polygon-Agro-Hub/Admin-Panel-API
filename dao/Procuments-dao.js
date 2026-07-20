ee98a6052da946e99d283240aa5cdd47d537f42e
DumithLusena <dumithlusena123@gmail.com>
2026-07-20 16:27:57 +0530

    fix: update processCreatedAt query and enhance image upload error handling

--- dao/Procuments-dao.js
+++ dao/Procuments-dao.js
@@ -1218,7 +1218,7 @@ exports.getAllOrdersWithProcessInfoDispatched = (page, limit, dateFilter, search
           po.invNo,
           po.status,
           po.createdAt,
-          (SELECT ADDTIME(MAX(opi.createdAt), '05:30:00') FROM orderpackageitems opi WHERE opi.orderPackageId = op.id) AS processCreatedAt,
+          (SELECT MAX(opi.createdAt) FROM orderpackageitems opi WHERE opi.orderPackageId = op.id) AS processCreatedAt,
           op.packingStatus,
           au.userName
         FROM processorders po
